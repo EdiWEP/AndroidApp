4605dfb6f4a59e0698ba6e0b384e61c36c36d710
Edi Podaru <edipodaru@gmail.com>
2023-05-25 10:48:10 +0300

    Add share button to todo items

--- app/src/main/java/com/example/todo/TodoAdapter.kt
+++ app/src/main/java/com/example/todo/TodoAdapter.kt
@@ -2,11 +2,13 @@ package com.example.todo
 
 
 import android.content.Context
+import android.content.Intent
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.CheckBox
 import android.widget.TextView
 import androidx.core.content.ContextCompat
@@ -19,8 +21,9 @@ class TodoAdapter(
 ) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
 
     class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tvTodoTitle : TextView = itemView.findViewById(R.id.textviewTodoTitle)
+        val tvTodoTitle: TextView = itemView.findViewById(R.id.textviewTodoTitle)
         val checkboxDone: CheckBox = itemView.findViewById(R.id.checkboxDone)
+        val shareButton: Button = itemView.findViewById(R.id.shareButton)
     }
 
     fun addTodo(todo: Todo) {
@@ -52,6 +55,14 @@ class TodoAdapter(
         )
     }
 
+    private fun getTextToShare(holder: TodoViewHolder): String {
+        if (holder.checkboxDone.isChecked) {
+            return "I completed this task: ${holder.tvTodoTitle.text}"
+        }
+        else {
+            return "I am working on this task: ${holder.tvTodoTitle.text}"
+        }
+    }
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         val currentTodo = todos[position]
         holder.tvTodoTitle.text = currentTodo.title
@@ -61,6 +72,14 @@ class TodoAdapter(
         holder.checkboxDone.setOnCheckedChangeListener { _, _ ->
             toggleCheckboxColor(holder.checkboxDone)
             currentTodo.isDone = !currentTodo.isDone
+        }
+        holder.shareButton.setOnClickListener {
+            val textToShare = getTextToShare(holder)
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, textToShare)
+            val chooser = Intent.createChooser(intent, "Share via")
+            ContextCompat.startActivity(context, chooser, null)
         }
 
     }
